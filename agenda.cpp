964920767602aad65f1d8b8a944a5470547aece5
MaioCiranda <111889111+MaioCiranda@users.noreply.github.com>
2025-01-20 17:30:30 -0300

    Update agenda.cpp

--- agenda.cpp
+++ agenda.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-//teste// consegue ver?//
-//lembrar de ajeitar o layout no fim do exc!!!!!!!!!//
+#include <time.h>
+#include <ctype.h>
+
+const int LIMITE_PACIENTES = 50;
+const int LIMITE_CARACTERES = 51;
+const int IDADE_MINIMA = 13;
+const int LIMITE_CONSULTAS = 100;
+
 struct paciente
 {
   long long cpf;
-  char nome[50];
+  char nome[LIMITE_CARACTERES];
   int nascimento;
 };
 
@@ -20,16 +26,117 @@ struct consulta
 
 struct data
 {
-  int dia;
-  int mes;
   int ano;
-};
-struct hora
-{
-  int minutos;
-  int horas;
+  int mes;
+  int dia;
+  int hora;
+  int min;
+  int seg;
 };
 
+//Funções auxiliares
+
+bool cpf_valido(long long cpf)
+{
+  int primeiro_dv, segundo_dv, soma, resto, j, k;
+  long long numero_cpf;
+  int mult[10] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+  if (cpf < 11111111111L || cpf > 99999999999L || cpf % 11111111111L == 0)
+    return false;
+
+  primeiro_dv = cpf % 100 / 10,
+  segundo_dv  = cpf % 10;
+
+  numero_cpf = cpf / 100;
+  soma = 0;
+  for (int i = 0; i < 9; i++)
+  {
+    soma += (numero_cpf % 10) * mult[i];
+    numero_cpf /= 10;
+  }
+
+  resto = soma % 11;
+
+  if (resto == 0 || resto == 1)
+    j = 0;
+  else
+    j = 11 - resto;
+
+  if (j != primeiro_dv)
+    return false;
+
+  numero_cpf = cpf / 10;
+  soma = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    soma += (numero_cpf % 10) * mult[i];
+    numero_cpf /= 10;
+  }
+
+  resto = soma % 11;
+
+  if (resto == 0 || resto == 1)
+    k = 0;
+  else
+    k = 11 - resto;
+
+  if (k != segundo_dv)
+    return false;
+
+  return true;
+}
+
+void data_hora_atual(int &dia, int &mes, int &ano,
+int &hora, int &min, int &seg) 
+{
+  time_t t = time(NULL);
+  struct tm lt = *localtime(&t);
+  ano = lt.tm_year + 1900;
+  mes = lt.tm_mon + 1;
+  dia = lt.tm_mday;
+  hora = lt.tm_hour;
+  min = lt.tm_min;
+  seg = lt.tm_sec;
+}
+
+data int_para_data (int data_int)
+{
+  data data_struct;
+  data_struct.ano = data_int % 10000;
+  data_struct.mes = (data_int % 1000000) / 10000;
+  data_struct.dia = data_int / 1000000;
+  return data_struct;
+}
+
+bool idade_minima (int nascimento, int idade_minima)
+{
+  void data_hora_atual(int &dia, int &mes, int &ano,
+  int &hora, int &min, int &seg);
+  data data_atual;
+  data_hora_atual(data_atual.dia, data_atual.mes, data_atual.ano, data_atual.hora,
+                  data_atual.min, data_atual.seg);
+
+  data nascimento_formatado = int_para_data(nascimento);
+  if ((data_atual.ano - nascimento_formatado.ano) * 365 +
+      (data_atual.mes - nascimento_formatado.mes) * 30 +
+       data_atual.dia - nascimento_formatado.dia < idade_minima * 365)
+    return false;
+  return true;
+}
+
+// Função que retorna a posição do CPF na lista ou -1 para cpfs não cadastrados
+int posicao_paciente(paciente lista[LIMITE_PACIENTES], long long cpf)
+{
+  for (int i = 0; i < LIMITE_PACIENTES; i++)
+  {
+    if (lista[i].cpf == cpf)
+      return i;
+  }
+  return -1;
+}
+
+//Funções Principais
 
 int menu_principal()
 {
@@ -66,41 +173,50 @@ int menu_agenda()
 
 paciente cadastro_paciente()
 {
-  bool cpf_valido(long long cpf);
-  
   paciente paciente_novo;
-  int cpf;
-  printf("CPF: ");
-  scanf("%i", &cpf);
-  if (cpf_valido(cpf) == false)
-  {
-    printf("CPF inválido.");
-    return {-1, -1, -1};
-  }
-  else
-    paciente_novo.cpf = cpf;
+  int paciente_valido = 0;
 
-  char nome[50];
-  printf("Nome: ");
-  fgets(nome, 50, stdin);
-  if (strlen(nome) < 4)
+  // CPF
+  long long cpf;
+  while (cpf_valido(cpf) == false)
   {
-    printf("CPF inválido");
-    return {-1, -1, -1};
+    printf("CPF: ");
+    scanf("%lld", &cpf);
+    if (cpf_valido(cpf) == false)
+      printf("CPF inválido.\n");
   }
-  else
-    for (int i = 0; i < 50; i++)
-    {
-      paciente_novo.nome[i] = nome[i];
-      if (nome[i] == '\0')
-        break;
-    }
-    
-}
+  paciente_novo.cpf = cpf;
 
-void excluir_paciente()
-{
-  // A FAZER
+  // Nome
+  char nome[LIMITE_CARACTERES];
+  for (;;)
+  {
+    printf("Nome: ");
+    while (getchar() != '\n');
+    fgets(nome, LIMITE_CARACTERES, stdin);
+    if (strlen(nome) < 4)
+      printf("Nome inválido\n");
+    else
+      break;
+  }
+  int i = 0;
+  while (nome[i] != '\0')
+  {
+    paciente_novo.nome[i] = toupper(nome[i]);
+    i++;
+  }
+  
+  // Data de Nascimento
+  int data_nascimento = 99999999;
+  while (idade_minima(data_nascimento, IDADE_MINIMA) == false)
+  {
+    printf("Data de nascimento: ");
+    scanf(" %i", &data_nascimento);
+    if (idade_minima(data_nascimento, IDADE_MINIMA) == false)
+      printf("Apenas pacientes acima de %i anos\n", IDADE_MINIMA);
+  }
+  paciente_novo.nascimento = data_nascimento;
+  return paciente_novo;
 }
 
 void listar_pacientes_por_cpf()
@@ -113,46 +229,10 @@ void listar_pacientes_por_nome()
   // A FAZER
 }
 
-int data()
-
 void agendar_consulta()
 {
- for (int i = 0; i < 50; i ++)
-{
-    if (cpf == lista_pacientes[i].cpf)
-      printf("true");
-    else
-      printf("false");
+  // A FAZER
 }
-}
-int data_valida()
-{
-  data data_correta;
-  do
-  {
-  printf("DATA\n Dia Mes Ano");
-  scanf("%d %d %d". &data_correta.dia, &data_correta.mes, &data_correta.ano);
-  if (data_correta.dia < 1 || data_correta.dia >= 32 && data_correta.mes < 1 || data_correta.mes >= 13 && data_correta.ano < 2025)
-    printf("ERRO");
-    continue;
-  } while (data_cocorretarreta.dia < 1 || data_correta.dia >= 32 && data_correta.mes < 1 || data_correta.mes >= 13 && data_correta.ano < 2025)
-  return int data_cor;
-  
-}
- 
-int hora_valida()
-{
-  do
-  {
-  printf("Hora\n Horas Minutos");
-  scanf("%d %d". &hora.horas, &hora.minuto);
-  if (hora.horas < 8 || hora.horas > 19 && hora.minuto < 1 || hora.minuto >= 60 )
-    printf("ERRO");
-    continue;
-  } while (hora.horas < 8 || hora.horas > 19 && hora.minuto < 1 || hora.minuto >= 60 )
-  return int hora;
-}
-
 
 void cancelar_agendamento()
 {
@@ -164,91 +244,15 @@ void listar_agenda()
   // A FAZER
 }
 
-//Funções auxiliares
-
-bool cpf_valido(long long cpf)
-{
-    int primeiro_dv, segundo_dv, soma, resto, j, k;
-    long long numero_cpf;
-    int mult[10] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-
-    // Se cpf n�o est� no intervalo v�lido ou se tem todos os d�gitos iguais
-    // ent�o � inv�lido
-    if (cpf < 11111111111L || cpf > 99999999999L || cpf % 11111111111L == 0)
-        return false;
-
-    // Pega o primeiro DV e o segundo DV do cpf
-    primeiro_dv = cpf % 100 / 10,
-    segundo_dv  = cpf % 10;
-
-    // Calcula primeiro DV
-    numero_cpf = cpf / 100; // N�mero do cpf sem os DVs
-    soma = 0;
-    for (int i = 0; i < 9; i++)
-    {
-        soma += (numero_cpf % 10) * mult[i];
-        numero_cpf /= 10;
-    }
-
-    resto = soma % 11;
-
-    if (resto == 0 || resto == 1)
-        j = 0;
-    else
-        j = 11 - resto;
-
-    // Compara o primeiro DV calculado com o informado
-    // Se � inv�lido, ent�o nem precisa calcular o segundo
-    if (j != primeiro_dv)
-        return false;
-
-    // Calcula segundo DV
-    numero_cpf = cpf / 10;  // N�mero do cpf sem segundo DV
-    soma = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        soma += (numero_cpf % 10) * mult[i];
-        numero_cpf /= 10;
-    }
-
-    resto = soma % 11;
-
-    if (resto == 0 || resto == 1)
-        k = 0;
-    else
-        k = 11 - resto;
-
-    // Compara o segundo DV calculado com o informado
-    if (k != segundo_dv)
-        return false;
-
-    return true;
-}
-
 int main()
 {
-    char resp;
-    long long cpf;
+  paciente lista_pacientes[LIMITE_PACIENTES];
+  for (int i = 0; i < LIMITE_PACIENTES; i++)
+  {
+    lista_pacientes[i].cpf = 0;
+  }
 
-    do {
-        printf("CPF: ");
-        scanf("%lld", &cpf);
-        getchar();
-
-        if (cpf_valido(cpf))
-            puts("Valido");
-        else
-            puts("Invalido");
-
-        printf("Continua (S/N)? ");
-        resp = getchar();
-    } while (resp == 's' || resp == 'S');
-}
-
-int main()
-{
-  paciente lista_pacientes[50];
-  consulta lista_consultas[100];
+  consulta lista_consultas[LIMITE_CONSULTAS];
   
   int requerimento;
   
@@ -266,13 +270,42 @@ int main()
       {
         case 1:
         {
-          paciente temp = cadastro_paciente();
-          printf("%i", temp.cpf);
+          bool tem_invalido = false;
+          paciente paciente_temp = cadastro_paciente();
+          for (int i = 0; i < LIMITE_PACIENTES; i++)
+          {
+            if (paciente_temp.cpf == lista_pacientes[i].cpf)
+            {
+              printf("\nPaciente já cadastrado");
+              tem_invalido = true;
+              break;
+            }
+            if (lista_pacientes[i].cpf == 0)
+            {
+              lista_pacientes[i] = paciente_temp;
+              printf("\nPaciente cadastrado com sucesso!\n");
+              tem_invalido = true;
+              break;
+            }
+          }
+          if (tem_invalido == false)
+            printf("\nLimite de pacientes atingido\n");
+          break;
+        } 
+        
+        case 2:
+        {
+          long long cpf_a_excluir;
+          printf("CPF: ");
+          scanf("%lld", &cpf_a_excluir);
+          int n = posicao_paciente(lista_pacientes, cpf_a_excluir);
+          if (n == -1)
+            printf("\nPaciente não cadastrado");
+          else
+            lista_pacientes[n].cpf = 0;
           break;
         }
-        case 2:
-          excluir_paciente();
-          break;
+
         case 3:
           listar_pacientes_por_cpf();
           break;
@@ -295,8 +328,6 @@ int main()
       {
         case 1:
           agendar_consulta();
-          
-          
           break;
         case 2:
           cancelar_agendamento();
